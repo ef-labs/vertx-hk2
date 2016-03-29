1969fe9c0f1573197745ef629b9bd49d975ec0c1
Adrian Gonzalez <adrian.gonzalez@ef.com>
2016-03-29 15:42:51 -0400

    Add vertx-hk2-simple example module

---
+++ examples/vertx-hk2-simple/src/main/java/com/englishtown/vertx/hk2/examples/MyDependency.java
@@ -0,0 +1,7 @@
+package com.englishtown.vertx.hk2.examples;
+
+/**
+ * An injectable dependency
+ */
+public interface MyDependency {
+}
---
+++ examples/vertx-hk2-simple/src/main/java/com/englishtown/vertx/hk2/examples/impl/MyDependencyImpl.java
@@ -0,0 +1,9 @@
+package com.englishtown.vertx.hk2.examples.impl;
+
+import com.englishtown.vertx.hk2.examples.MyDependency;
+
+/**
+ * Default implementation of {@link MyDependency}
+ */
+public class MyDependencyImpl implements MyDependency {
+}
